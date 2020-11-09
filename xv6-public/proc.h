ab1383375895f1efed32b88535878b206e713dac
Alexander Ku <aku006@sledge.cs.ucr.edu>
2020-11-09 13:32:37 -0800

    Added setPriority function stuff to other files, need to work on actual function

--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int priority;			//Hold the value of task priority, custom-added variable
 };
 
 // Process memory is laid out contiguously, low addresses first:
